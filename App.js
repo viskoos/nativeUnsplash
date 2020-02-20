a8fa887411435c88ba18099243dad8b2a7c3719f
potniye@gmail.com <qs_nikita@rocketfirm.com>
2020-02-20 23:09:07 +0600

    feat(app): add axios featching

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react'
+import {View, TextInput, Text, Image} from 'react-native'
+import axios from 'axios'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const clientId = '09465ee0b43bf274a0c67732a9a8f75faad53b2495ee077947cf0fca6a68bfe5'
+const url = 'https://api.unsplash.com/search/photos?page=1&query=cats&client_id=09465ee0b43bf274a0c67732a9a8f75faad53b2495ee077947cf0fca6a68bfe5'
+
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      text: '',
+      imgs: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(url)
+      .then((response) => {
+        console.log(response)
+      })
+  }
+
+  render() {
+    return (
+      <View style={{justifyContent: "center", alignItems: 'center', paddingTop: 30, paddingLeft: 20, paddingRight: 20}}>
+        <TextInput
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+          placeholder="Search photo"
+        />
+        <Text >
+        </Text>
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
